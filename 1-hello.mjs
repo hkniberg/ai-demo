8cabe399692d9eb02524a8e2c6536b63217e95d0
Henrik Kniberg <h@kniberg.com>
2023-09-05 11:17:24 +0200

    Initial commit

---
+++ 1-hello.mjs
@@ -0,0 +1,16 @@
+import {OpenAI} from "openai";
+import {config} from "dotenv";
+config();
+const openai = new OpenAI({apiKey: process.env.OPENAI_KEY});
+
+async function main() {
+    const result = await openai.chat.completions.create({
+        model: "gpt-4",
+        messages: [
+            {"role": "user", "content": "hi there"},
+        ]
+    })
+    console.log(result.choices[0].message);
+}
+
+main();
