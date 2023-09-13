77f11869bc14cdb7460a0812224cfab73bc3ab88
Henrik Kniberg <h@kniberg.com>
2023-09-13 11:21:25 +0200

    Image demo

--- src/image-demo/image-demo.mjs
+++ src/image-demo/image-demo.mjs
@@ -3,9 +3,10 @@ import {config} from "dotenv";
 config({path: '../.env'});
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
+// https://platform.openai.com/docs/api-reference/images/create
 const result = await openai.images.generate({
     prompt: "An ugly cat",
-    size: "256x256"
+    size: "256x256" // optional, defaults to 1024x1024
 });
 
 console.log(result.data[0].url);
