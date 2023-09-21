ea077109ea922b55055f2bd63abaac579235fff6
Henrik Kniberg <h@kniberg.com>
2023-09-21 18:13:05 +0200

    Image chat demo

---
+++ src/image-chat-demo/image-chat-demo-1.mjs
@@ -0,0 +1,60 @@
+import { OpenAI } from "openai";
+import { config } from "dotenv";
+import readlineSync from "readline-sync";
+config({ path: '../../.env' });
+
+/*
+Prompt used to generate this:
+
+Here is simple chat application using OpenAI:
+...(chat-demo/2-chat-system-prompt-mjs)...
+
+Here is sample code for generating an image URL from a prompt:
+...(image-demo/image-demo.mjs)...
+
+Create a new chat application that is image-enabled
+It is like chatgpt, but you can also ask it generate images.
+
+ */
+
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
+const messages = [
+    { role: "system", content: "you are a helpful assistant" },
+];
+
+async function chat() {
+    console.log("Write 'generate image: xxx' to generate an image.")
+    while (true) {
+        const input = readlineSync.question("You: ");
+        messages.push({ role: "user", content: input });
+
+        if (input.toLowerCase().startsWith('generate image:')) {
+            const prompt = input.substring('generate image:'.length).trim();
+            try {
+                const result = await openai.images.generate({
+                    prompt: prompt,
+                    size: "256x256"
+                });
+                console.log(`GPT: Here is the image URL: ${result.data[0].url}`);
+            } catch (error) {
+                console.error(`GPT: Sorry, I couldn't generate the image. ${error.message}`);
+            }
+        } else {
+            try {
+                const result = await openai.chat.completions.create({
+                    model: "gpt-3.5-turbo",
+                    messages: messages,
+                });
+
+                const reply = result.choices[0].message;
+                console.log(`GPT: ${reply.content}`);
+                messages.push(reply);
+            } catch (error) {
+                console.error(`GPT: Sorry, I couldn't generate a response. ${error.message}`);
+            }
+        }
+    }
+}
+
+chat();
---
+++ src/image-chat-demo/image-chat-demo-2.mjs
@@ -0,0 +1,88 @@
+import { OpenAI } from "openai";
+import { config } from "dotenv";
+import readlineSync from "readline-sync";
+config({ path: '../../.env' });
+
+
+/*
+Prompt used to generate this:
+
+Here is simple chat application using OpenAI:
+...(chat-demo/2-chat-system-prompt-mjs)...
+
+Here is sample code for generating an image URL from a prompt:
+...(image-demo/image-demo.mjs)...
+
+Create a new chat application that is image-enabled.
+It is like chatgpt, but you can also ask it generate images.
+
+For example:
+Me: "Hi there"
+GPT: "How can I help you?"
+Me: "I need a picture of a pig"
+GPT: OK, here you go: (image URL)
+Me: "Actually, I want it to wear a hat"
+GPT: OK, here is a pig with a hat: (image URL)
+
+Use gpt-4 instead of gpt-3.5-turbo.
+Use openai.chat to determine if the user is asking for an image, and to generate the image prompt in that case.
+Use openai.images to generate the image URL from the image prompt.
+
+Use the following system prompt:
+"You are a chatbot that can also generate images. When an image needs to be generated,
+include a string like IMAGE[<image prompt>].
+
+Example:
+User: I want an image of a pig, it should have a hat
+Assistant: Sure, I can do that for you. IMAGE[pig with hat]
+
+Then send 'pig with hat' to openai.images, and replace the prompt with the URL, so the output is:
+ 'Sure, I can do that for you. http://....'
+
+ Also log the image prompt.
+ */
+const openai = new OpenAI({ apiKey: process.env.OPENAI_KEY });
+
+const messages = [
+    { role: "system", content: "You are a chatbot that can also generate images. When an image needs to be generated, include a string like IMAGE[<image prompt>]." },
+];
+
+async function generateImage(prompt) {
+    try {
+        const result = await openai.images.generate({
+            prompt: prompt,
+            size: "256x256"
+        });
+        return result.data[0].url;
+    } catch (error) {
+        console.error("Error generating image:", error);
+        return "Error generating image.";
+    }
+}
+
+async function chat() {
+    while (true) {
+        const input = readlineSync.question("You: ");
+        messages.push({ role: "user", content: input });
+
+        const result = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: messages,
+        });
+
+        let reply = result.choices[0].message.content;
+        const imagePromptMatch = reply.match(/IMAGE\[(.*?)\]/);
+
+        if (imagePromptMatch) {
+            const imagePrompt = imagePromptMatch[1];
+            console.log(`Image Prompt: ${imagePrompt}`);
+            const imageUrl = await generateImage(imagePrompt);
+            reply = reply.replace(`IMAGE[${imagePrompt}]`, imageUrl);
+        }
+
+        console.log(`GPT: ${reply}`);
+        messages.push({ role: "assistant", content: reply });
+    }
+}
+
+chat();
